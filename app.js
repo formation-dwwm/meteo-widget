77c02cdf128c877d8a4e5fb76efe8adbf42b112f
Matthias Gaudin <gatthias@gmail.com>
2019-12-18 19:48:18 +0100

    Add 4 days forecast display.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const $cityName = document.getElementById("city-name"),
       $currentDate = document.getElementById("current-date"),
       $currentConditionIcon = document.getElementById("current-condition-icon"),
       $currentConditionLabel = document.getElementById("current-condition-label"),
-      $currentTemp = document.getElementById("current-temp");
+      $currentTemp = document.getElementById("current-temp"),
+      $forecastList = document.getElementById("forecast-list");
 
 function updateWeather(cityName){
   fetch(`https://www.prevision-meteo.ch/services/json/${cityName}`)
@@ -12,11 +13,37 @@ function updateWeather(cityName){
 }
 
 function updateView(data){
+  // Update current conditions
   $cityName.textContent = data.city_info.name;
   $currentDate.textContent = data.current_condition.date;
   $currentTemp.textContent = data.current_condition.tmp;
   $currentConditionLabel.textContent = data.current_condition.condition;
   $currentConditionIcon.setAttribute("src", data.current_condition.icon);
+
+  updateForecastView(data);
+}
+
+function updateForecastView(data){
+  let html = "";
+
+  for(let dayNum = 1; dayNum <= 4; ++dayNum){
+    const dayKey = "fcst_day_" + dayNum;
+    const dayData = data[dayKey];
+    html += createForecastItemHtml(dayData, dayNum);
+  }
+
+  $forecastList.innerHTML = html;
+}
+
+function createForecastItemHtml(data, dayNum){
+  return `
+<li class="forecast-item">
+  <span class="date">${data.day_long}</span>
+  <img class="icon" src="${data.icon}"/>
+  <span class="temp temp-max">${data.tmax}</span>
+  <span class="temp temp-min">${data.tmin}</span>
+</li>
+  `;
 }
 
 function handleError(err){
