11c0367ee911ef30523c96b9eeb6912351e50bdf
Matthias Gaudin <gatthias@gmail.com>
2020-01-05 17:05:30 +0100

    Refactor: Add interfaces.

--- src/MeteoWidget.ts
+++ src/MeteoWidget.ts
@@ -1,3 +1,110 @@
+interface ICityInfoData {
+  name: string;
+  country: string;
+  latitude: string;
+  longitude: string;
+  elevation: string;
+  sunrise: string;
+  sunset: string;
+}
+
+interface IForecastInfoData {
+  latitude: string | null;
+  longitude: string | null;
+  elevation: string;
+}
+
+interface ICurrentConditionsData {
+  date: string;
+  hour: string;
+  tmp: number;
+  wnd_spd: number;
+  wnd_gust: number;
+  wnd_dir: string;
+  pressure: number;
+  humidity: number;
+  condition: string;
+  condition_key: string;
+  icon: string;
+  icon_big: string;
+}
+
+interface IForecastHourData {
+  ICON: string;
+  CONDITION: string;
+  CONDITION_KEY: string;
+  TMP2m: number;
+  DPT2m: number;
+  WINDCHILL2m: number;
+  HUMIDEX: number;
+  RH2m: number;
+  APCPsfc: number;
+  WNDSPD10m: number;
+  WNDGUST10m: number;
+  WNDDIR10m: number;
+  string;
+  ISSNOW: number;
+  HCDC: string;
+  MCDC: string;
+  LCDC: string;
+  HGT0C: number;
+  KINDEX: number;
+  CAPE180_0: string;
+  CIN180_0: number;
+}
+
+interface IHourlyData {
+  "0H00": IForecastHourData;
+  "1H00": IForecastHourData;
+  "2H00": IForecastHourData;
+  "3H00": IForecastHourData;
+  "4H00": IForecastHourData;
+  "5H00": IForecastHourData;
+  "6H00": IForecastHourData;
+  "7H00": IForecastHourData;
+  "8H00": IForecastHourData;
+  "9H00": IForecastHourData;
+  "10H00": IForecastHourData;
+  "11H00": IForecastHourData;
+  "12H00": IForecastHourData;
+  "13H00": IForecastHourData;
+  "14H00": IForecastHourData;
+  "15H00": IForecastHourData;
+  "16H00": IForecastHourData;
+  "17H00": IForecastHourData;
+  "18H00": IForecastHourData;
+  "19H00": IForecastHourData;
+  "20H00": IForecastHourData;
+  "21H00": IForecastHourData;
+  "22H00": IForecastHourData;
+  "23H00": IForecastHourData;
+}
+
+interface IForecastDayData {
+  date: string;
+  day_short: string;
+  day_long: string;
+  tmin: number;
+  tmax: number;
+  condition: string;
+  condition_key: string;
+  icon: string;
+  icon_big: string;
+  hourly_data: IHourlyData;
+}
+
+interface IWeatherData{
+  city_info: ICityInfoData;
+  forecast_info: IForecastInfoData;
+  current_condition: ICurrentConditionsData;
+  fcst_day_0: IForecastDayData;
+  fcst_day_1: IForecastDayData;
+  fcst_day_2: IForecastDayData;
+  fcst_day_3: IForecastDayData;
+  fcst_day_4: IForecastDayData;
+}
+
+
 class MeteoWidget extends HTMLElement {
 
   protected cityName: string = "paris";
@@ -13,25 +120,29 @@ class MeteoWidget extends HTMLElement {
     return ['city'];
   }
   
-  public connectedCallback(){
+  public connectedCallback(): void {
     this.init();
 
     this.updateWeather();
   }
 
-  public attributeChangedCallback(name: string, oldValue: string, newValue: string) {
-    if(name == "city" && oldValue != newValue){
+  public attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
+    if(name == "city" && oldValue != newValue && newValue != ""){
       this.setCity(newValue);
     }
   }
 
-  public setCity(cityName: string){
+  public setCity(cityName: string): void {
+    if(this.cityName == cityName){
+      return;
+    }
+    
     this.cityName = cityName;
 
     this.updateWeather();
   }
 
-  protected init(){
+  protected init(): void {
     this.root = this.attachShadow({mode: 'open'});
     this.root.innerHTML = `
     <style>
@@ -48,24 +159,24 @@ class MeteoWidget extends HTMLElement {
   }
 
 
-  protected updateWeather(){
+  protected updateWeather(): void {
     fetch(`https://www.prevision-meteo.ch/services/json/${this.cityName}`)
-      .then(res => res.json())
-      .then(data => this.updateView(data))
-      .catch(err => this.handleError(err));
+      .then((res: Response) => res.json())
+      .then((data: IWeatherData) => this.updateView(data))
+      .catch((err: Error) => this.handleError(err));
   }
 
-  protected updateView(data){
+  protected updateView(data: IWeatherData): void {
     // Update current conditions
     this.$cityName.textContent = data.city_info.name;
-    this.$currentTemp.textContent = data.current_condition.tmp;
+    this.$currentTemp.textContent = ''+data.current_condition.tmp;
     this.$currentConditionLabel.textContent = data.current_condition.condition;
     this.$currentConditionIcon.setAttribute("src", data.current_condition.icon);
   
     this.updateForecastView(data);
   }
   
-  protected updateForecastView(data){
+  protected updateForecastView(data: IWeatherData): void {
     let html = "";
   
     for(let dayNum = 1; dayNum <= 4; ++dayNum){
@@ -77,7 +188,7 @@ class MeteoWidget extends HTMLElement {
     this.$forecastList.innerHTML = html;
   }
   
-  protected createForecastItemHtml(data, dayNum: number): string{
+  protected createForecastItemHtml(data: IForecastDayData, dayNum: number): string {
     return `
       <li class="forecast-item">
         <span class="date">${data.day_long}</span>
@@ -90,7 +201,7 @@ class MeteoWidget extends HTMLElement {
     `;
   }
   
-  protected handleError(err: Error){
+  protected handleError(err: Error): void {
     console.error(err);
   }
 
